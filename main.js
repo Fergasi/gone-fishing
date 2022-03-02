b65499f87b4fcf1371d98d6040f98a329c81050d
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-03-02 12:08:12 -0500

    v3.0

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-const fish = generateRandomFish();
-
 function generateRandomFish(){
 
     let name = generateRandomFishName();
@@ -17,7 +15,7 @@ function generateRandomFish(){
 
 function generateRandomFishName(){
     
-    let fishAdj1 = ['spotted', 'striped', 'southern', 'northern', 'pacific', 'atlantic', 'arctic', 'warmwater', 'speckled', 'cape'];
+    let fishAdj1 = ['californian', 'south african', 'chilean', 'spotted', 'striped', 'southern', 'northern', 'pacific', 'atlantic', 'arctic', 'warmwater', 'speckled', 'cape'];
     let fishAdj2 = ['black', 'bluefin', 'red', 'white', 'great', 'king', 'dogtooth', 'giant', 'yellowfin', 'golden', 'silver', 'yellow', 'blue'];
     let fishType = ['seabass', 'perch', 'kingfish', 'mackeral', 'salmon', 'tuna', 'barracuda', 'marlin', 'sailfish', 'queenfish', 'bonito', 'sardine', 'dorado', 'snapper', 'sturgeon'];
 
@@ -58,23 +56,26 @@ hour = 5 + Math.floor(time/60);
 min = time % 60;
 if (min < 10) {min = "0" + min;};
 
-
 console.log(`The time is ${hour}:${min}am`);
 console.log(' ');
 console.log("So far you've caught:")
 console.log(catchNum + ' fish, with a total weight of ' + totalCatchWeight.toFixed(2) + ' lbs, and a total value of $' + totalCatchValue.toFixed(2));
 console.log(' ');
 
-caughtFish.push(generateRandomFish());
+let randomFish = generateRandomFish()
 
-console.log('You just caught a ' + caughtFish[caughtFish.length-1].name + ' weighing ' + caughtFish[caughtFish.length-1].weight + 'lbs, with a value of $' + caughtFish[caughtFish.length-1].value)
+console.log('You just caught a ' + randomFish.name + ' weighing ' + randomFish.weight + 'lbs, with a value of $' + randomFish.value)
 
-totalCatchWeight += caughtFish[caughtFish.length-1].weight;
-totalCatchValue += caughtFish[caughtFish.length-1].value;
-catchNum = caughtFish.length;
+catchReleasePrompt = prompt('Your action: [c]atch or [r]elease?' )
 
+if (catchReleasePrompt === 'c'){
 
+    caughtFish.push(randomFish);
 
+    totalCatchWeight += Number(randomFish.weight);
+    totalCatchValue += Number(randomFish.value);
+    catchNum = caughtFish.length;
 
+}
 
 }
